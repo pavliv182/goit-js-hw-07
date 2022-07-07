49190cd185c3b31d8e937500fabe5c23f035461c
pavliv182 <uptn182@gmail.com>
2022-07-07 20:24:26 +0300

    simple light box done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,31 +2,18 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 const galleryEl = document.querySelector(".gallery");
 const galleryElements = galleryItems.map(
-  (el) => `<div class="gallery__item">
-  <a class="gallery__link" href="${el.original}">
-    <img
-      class="gallery__image"
-      src="${el.preview}"
-      data-source="${el.original}"
-      alt="${el.description}"
-    />
-  </a>
-</div>`
+  (el) => `<li class="gallery__item">
+  <a class="gallery__item" href="${el.original}">
+        <img 
+            class="gallery__image" 
+            src="${el.preview}" 
+            alt="${el.description}" 
+        />
+    </a>
+</li>`
 );
 galleryEl.innerHTML = galleryElements.join("");
-galleryEl.addEventListener("click", (event) => {
-  if (event.target.nodeName === "IMG") {
-    event.preventDefault();
-    const instance = basicLightbox.create(`
-      <img src="${event.target.dataset.source}" width="800" height="600">
-  `);
-    instance.show();
-    galleryEl.addEventListener("keydown", closeModal);
-    function closeModal(event) {
-      if (event.code === "Escape") {
-        instance.close();
-        galleryEl.removeEventListener("keydown", closeModal);
-      }
-    }
-  }
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionsDelay: 250,
 });
